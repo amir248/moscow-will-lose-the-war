0973c2a0ad61f661cb46da80d99c69359d8a419c
amir248 <chikchicly@yandex.ru>
2022-03-05 15:29:06 +0700

    6

--- maxWidth.js
+++ maxWidth.js
@@ -17,6 +17,6 @@ if(window.innerWidth>1100){
   <p style="background:rgba(0,100,0,0.3);margin:1%;border-radius:7px; text-align:center;"><a href='https://amir248.github.io/mAscow/'>мАсква</a></p>
   <p style="background:rgba(0,0,25,0.3);margin:1%;border-radius:7px; text-align:center;"><a href='https://amir248.github.io/moscow-will-lose-the-war/'>москва проиграет войну</a></p>
   <p style="background:rgba(100,100,0,0.3);margin:1%;border-radius:7px; text-align:center;"><a href='https://amir248.github.io/justification/'>оправдание войне</a></p>
-  <p style="background:rgba(10,100,0,0.3);margin:1%;border-radius:7px; text-align:center;"><a href='https://amir248.github.io/facking-moscow/'>facking-moscow</a></p>
+  <p style="background:rgba(0,0,100,0.3);margin:1%;border-radius:7px; text-align:center;"><a href='https://amir248.github.io/muscovite/'>мАсковщина</a></p>
   `;
 }
